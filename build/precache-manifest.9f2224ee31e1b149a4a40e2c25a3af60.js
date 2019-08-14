e2cd7a33abf57166ce13ddeaae085da822ec3833
mengxiangdun <mengxiangdun@126.com>
2019-08-14 10:33:39 +0800

    firstcommit

---
+++ build/precache-manifest.9f2224ee31e1b149a4a40e2c25a3af60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3698d6e388aea2e769f95dc0df7c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a4335921de40766feb",
+    "url": "/static/css/main.5294e486.chunk.css"
+  },
+  {
+    "revision": "ae74ce96ac67881bcfe8",
+    "url": "/static/js/2.7ffdcbb9.chunk.js"
+  },
+  {
+    "revision": "59a4335921de40766feb",
+    "url": "/static/js/main.0e32793f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
