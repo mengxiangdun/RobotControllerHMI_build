62c855b1e0ef5106926b646f80641535babf441e
mengxiangdun <mengxiangdun@126.com>
2019-08-16 12:35:48 +0800

    123

---
+++ build/precache-manifest.0288744d7af68d7d783318e87044ff72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6b30a5f6a12afb367efab6471394ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ae554017aa9c0eb8a83",
+    "url": "/static/css/main.53b8d1a3.chunk.css"
+  },
+  {
+    "revision": "e37980aa1da42c11cee1",
+    "url": "/static/js/2.d67bb502.chunk.js"
+  },
+  {
+    "revision": "2ae554017aa9c0eb8a83",
+    "url": "/static/js/main.19f855ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
