34450fc516cebf8987db64f2604876d3d364374c
liyuyuan6969 <lyoonai@163.com>
2019-12-08 17:40:14 +0800

    whiteColor主题UI-1208

---
+++ www/precache-manifest.6b7dad09dbd11954c66ad3b902fc81ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf755c45c16e67c86c494e00dd9c0c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78c2a589798f66e9a30a",
+    "url": "/static/css/main.41cdb693.chunk.css"
+  },
+  {
+    "revision": "d8a65f80ccbf462725ad",
+    "url": "/static/js/2.5b18238e.chunk.js"
+  },
+  {
+    "revision": "78c2a589798f66e9a30a",
+    "url": "/static/js/main.731739c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "10605d2999ef497c598b9be63fc7302b",
+    "url": "/static/media/seven_axes.10605d29.png"
+  },
+  {
+    "revision": "090ff2c1190ddfa40f26316947c66172",
+    "url": "/static/media/six_axes.090ff2c1.png"
+  }
+]);
--- www/service-worker.js
+++ www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b992b86245cb9388179bd80eff75da70.js"
+  "/precache-manifest.6b7dad09dbd11954c66ad3b902fc81ff.js"
 );
 
 self.addEventListener('message', (event) => {
