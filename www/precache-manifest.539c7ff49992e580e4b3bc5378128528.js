0494560b82aae9133d09889de9cf844cc9f5ff46
liyuyuan6969 <lyoonai@163.com>
2019-12-09 17:40:16 +0800

    1209-UI界面

---
+++ www/precache-manifest.539c7ff49992e580e4b3bc5378128528.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7272056ef4cbb5b2506d9cff369690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f2c9dcbca8e4a81735b",
+    "url": "/static/css/main.41cdb693.chunk.css"
+  },
+  {
+    "revision": "d8a65f80ccbf462725ad",
+    "url": "/static/js/2.5b18238e.chunk.js"
+  },
+  {
+    "revision": "0f2c9dcbca8e4a81735b",
+    "url": "/static/js/main.5f6bfe3b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "10605d2999ef497c598b9be63fc7302b",
+    "url": "/static/media/seven_axes.10605d29.png"
+  },
+  {
+    "revision": "090ff2c1190ddfa40f26316947c66172",
+    "url": "/static/media/six_axes.090ff2c1.png"
+  }
+]);
--- www/service-worker.js
+++ www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b992b86245cb9388179bd80eff75da70.js"
+  "/precache-manifest.539c7ff49992e580e4b3bc5378128528.js"
 );
 
 self.addEventListener('message', (event) => {
